e48ebd7d26fad38a6ad4c1694f9ed1ffdfbdea19
SlobodanSkaric <softverss@gmail.com>
2024-09-03 11:29:39 +0200

    Add small change on dashbord. Sorry this change on main branch

--- react/src/views/Dashbord.jsx
+++ react/src/views/Dashbord.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+
 
 export default function Dashbord() {
   return (
@@ -14,34 +15,34 @@ export default function Dashbord() {
                             <input className='rounded-lg px-3 pt-0 text-slate-700' type="text" placeholder='pretraga'/>
                         </div>
                     </div>
-                    <div className='w-full overflow-y-scroll pb-7 h-80'>
+                    <div className='w-full overflow-y-scroll pb-7 h-80 text-center'>
                         <ul>
-                            <li>1234567</li>
-                            <li>12345671</li>
+                            <li>123456711</li>
+                            <li>123456711</li>
                             <li>123456717</li>
-                            <li>1234567</li>
-                            <li>12345671</li>
+                            <li>123456711</li>
+                            <li>123456711</li>
                             <li>123456717</li>
-                            <li>1234567</li>
-                            <li>12345671</li>
+                            <li>123456711</li>
+                            <li>123456711</li>
                             <li>123456717</li>
-                            <li>1234567</li>
-                            <li>12345671</li>
+                            <li>123456711</li>
+                            <li>123456711</li>
                             <li>123456717</li>
-                            <li>1234567</li>
-                            <li>12345671</li>
+                            <li>123456711</li>
+                            <li>123456711</li>
                             <li>123456717</li>
-                            <li>1234567</li>
-                            <li>12345671</li>
+                            <li>123456711</li>
+                            <li>123456711</li>
                             <li>123456717</li>
-                            <li>1234567</li>
-                            <li>12345671</li>
+                            <li>123456711</li>
+                            <li>123456711</li>
                             <li>123456717</li>
-                            <li>1234567</li>
-                            <li>12345671</li>
+                            <li>123456711</li>
+                            <li>123456711</li>
                             <li>123456717</li>
-                            <li>1234567</li>
-                            <li>12345671</li>
+                            <li>123456711</li>
+                            <li>123456711</li>
                             <li>123456717</li>
                         </ul>
                     </div>
@@ -50,15 +51,65 @@ export default function Dashbord() {
         </div>
 
         <div className='text-white py-3 px-10 w-1/3 bg-slate-500 rounded-xl flex-1'>
-            <div className='flex'>
+            < div className='flex'>
                 <div className='text-center w-full'>
-                    <div className='text-xl mb-5 border-b-2'>Knizenje</div>
-                    <div className='flex justify-around'>
-                        <div>
+                    <div className='text-xl mb-5 border-b-2'>Info</div>
+                    <div className='flex justify-around'> 
+                        <table className='table-auto w-full material'> 
+                            <thead>
+                                <tr>
+                                    <td>
+                                        Materijal Stanje
+                                    </td>    
+                                    <td>
+                                        Isporuka
+                                    </td>                           
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        348810 - <span>10000 kom</span>
+                                    </td>
+                                    <td>
+                                        348810 - <span>10000 kom</span> - <span>27.08.2023</span>
+                                    </td>
+                                    
+                                </tr>
+                                <tr>
+                                    <td>
+                                        348810 - <span>10000 kom</span>
+                                    </td>
+                                    <td>
+                                        348810 - <span>10000 kom</span> - <span>27.08.2023</span>
+                                    </td>
+                                    
+                                </tr>
+                                <tr>
+                                    <td>
+                                        327082 - <span>20000 kom</span> 
+                                    </td>
+                                    <td>
+                                        327082 - <span>20000 kom</span> - <span>27.08.2023</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        327082 - <span>20000 kom</span> 
+                                    </td>
+                                    <td>
+                                        327082 - <span>20000 kom</span> - <span>27.08.2023</span>
+                                    </td>
+                                </tr>
+                                
+                            </tbody>
+                               
+                        </table>
+                       {/*  <div>
                             <div className='mb-3'>Materijal Stanje</div>
                             <div>
                                 <ul>
-                                    <li>348810 - <span>10000 kom</span></li>
+                                    <li></li>
                                     <li>348809 - <span>15000 kom</span></li>
                                     <li>327082 - <span>20000 kom</span></li>
                                 </ul>
@@ -75,11 +126,11 @@ export default function Dashbord() {
                                     <li>327082 - <span>20000 kom</span> - <span>27.08.2023</span></li>
                                 </ul>
                             </div>
-                        </div>
+                        </div>  */}
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </div> 
   )
 }
