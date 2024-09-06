c650b570f9bf52007acd9e9dc9472c490a08b906
SlobodanSkaric <softverss@gmail.com>
2024-09-06 11:42:27 +0200

    Add Histroy and Pass views

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -6,8 +6,8 @@ export default function DefaultLayout() {
     <div>
         <div className='flex px-11 py-7 justify-center text-center text-white gap-1'>
             <a href="/tickets" className="hover:bg-slate-300 hover:text-slate-100 text-slate-700 border-2 py-3 px-7">Nalozi</a>
-            <a href="#" className="bhover:bg-slate-300 hover:text-slate-100 text-slate-700 border-2 py-3 px-7">Istorija</a>
-            <a href="#" className="hover:bg-slate-300 hover:text-slate-100 text-slate-700 border-2 py-3 px-7">Knizenje</a>
+            <a href="/history" className="bhover:bg-slate-300 hover:text-slate-100 text-slate-700 border-2 py-3 px-7">Istorija</a>
+            <a href="/pass" className="hover:bg-slate-300 hover:text-slate-100 text-slate-700 border-2 py-3 px-7">Knizenje</a>
             <a href="/user" className="hover:bg-slate-300 hover:text-slate-100 text-slate-700 border-2 py-3 px-7">Slobodan Skaric</a>
         </div>
         <div>
