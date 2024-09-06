e257a784a92128b2bce002a33edc82450d95efb0
SlobodanSkaric <softverss@gmail.com>
2024-09-06 11:32:08 +0200

    Add Link

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -5,7 +5,7 @@ export default function DefaultLayout() {
   return (
     <div>
         <div className='flex px-11 py-7 justify-center text-center text-white gap-1'>
-            <a href="#" className="hover:bg-slate-300 hover:text-slate-100 text-slate-700 border-2 py-3 px-7">Nalozi</a>
+            <a href="/tickets" className="hover:bg-slate-300 hover:text-slate-100 text-slate-700 border-2 py-3 px-7">Nalozi</a>
             <a href="#" className="bhover:bg-slate-300 hover:text-slate-100 text-slate-700 border-2 py-3 px-7">Istorija</a>
             <a href="#" className="hover:bg-slate-300 hover:text-slate-100 text-slate-700 border-2 py-3 px-7">Knizenje</a>
             <a href="/user" className="hover:bg-slate-300 hover:text-slate-100 text-slate-700 border-2 py-3 px-7">Slobodan Skaric</a>
