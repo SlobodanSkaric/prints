99fa788856cfafae024ef652e7a2d56d1f452d51
SlobodanSkaric <softverss@gmail.com>
2024-09-11 19:06:07 +0200

    Implement basic pss

--- react/src/views/Pass.jsx
+++ react/src/views/Pass.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function Pass() {
   return (
-    <div>Knizenje</div>
+    <div>
+      <div>
+        <div className='flex  justify-center'>
+          <div>
+            <form action="">
+              <div className='flex flex-col'>
+              <label htmlFor="brpass">Broj Naloga</label>
+              <input type="text" id='brpass' className='border-solid'/>
+              </div>              
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
