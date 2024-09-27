16077437649ce4503a0641d938060e4c7b1083e5
SlobodanSkaric <slobodan.skaric@gmail.com>
2024-09-27 10:16:02 +0200

    Small change in ticket controller

--- app/Http/Controllers/Api/TicketsController.php
+++ app/Http/Controllers/Api/TicketsController.php
@@ -22,18 +22,19 @@ class TicketsController extends Controller
         $validation = $request->validated();
         $user = Auth::user();
 
-       try{ $tickets = new Ticket();
+       try{
+            $tickets = new Ticket();
             $tickets->tickets_number = $validation["tickets_number"];
             $tickets->pri_348810 = isset($validation["pri_348810"]) ? $validation["pri_348810"]: null;
             $tickets->pri_327120 = isset($validation["pri_327120"]) ? $validation["pri_327120"] : null;
-            $tickets->pri_521342 = isset($validation["pri_521342"]) ? $validation["pri_521342"] : null; 
+            $tickets->pri_521342 = isset($validation["pri_521342"]) ? $validation["pri_521342"] : null;
             $tickets->commnets = $validation["commnets"];
             $tickets->description =$validation["description"];
             $tickets->user_id = Auth::id();
 
             $tickets->save();
 
-        return response()->json(['message' => 'Ticket created successfully', 'ticket' => $tickets], 201);
+            return response()->json(['message' => 'Ticket created successfully', 'ticket' => $tickets], 201);
         }catch(QueryException $e){
             if($e->errorInfo[1] == 1062){
                 return response()->json(["message" => "This ticket is existe"], 422);
@@ -41,7 +42,7 @@ class TicketsController extends Controller
 
             return response()->json(['error' => 'Something went wrong.'], 500);
         }
-        
+
 
     }
 }
